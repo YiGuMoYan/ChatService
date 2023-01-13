a39922cf49215b80de97697630cc13b712c8baf2
YiGuMoYan <3194775246@qq.com>
2023-01-13 17:08:53 +0800

    add fill config

---
+++ src/main/java/top/yigumoyan/chat/config/MetaObjectHandlerConfig.java
@@ -0,0 +1,24 @@
+package top.yigumoyan.chat.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MetaObjectHandlerConfig implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtCreate", new Date(), metaObject);
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+
+    // 更新时的填充策略
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("gmtModified", new Date(), metaObject);
+    }
+}
