7824de4f4114b27744f69db4fdec2ef020d00842
YiGuMoYan <3194775246@qq.com>
2023-01-14 22:07:36 +0800

    isToken ok

--- src/main/java/top/yigumoyan/chat/utils/TokenUtils.java
+++ src/main/java/top/yigumoyan/chat/utils/TokenUtils.java
@@ -1,12 +1,36 @@
 package top.yigumoyan.chat.utils;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import top.yigumoyan.chat.entity.Account;
+import top.yigumoyan.chat.service.Impl.AccountServiceImpl;
 
+import javax.annotation.Resource;
 import java.util.Date;
 
 import static top.yigumoyan.chat.utils.AESUtils.Encrypt;
 
+@Component
 public class TokenUtils {
+
+    private static AccountServiceImpl accountService;
+
+    @Autowired
+    public void setAccountService(AccountServiceImpl accountService) {
+        TokenUtils.accountService = accountService;
+    }
+
+    public static Boolean isToken(Account account) {
+        // 验证 token
+        QueryWrapper<Account> queryWrapperToken = new QueryWrapper<>();
+        queryWrapperToken.select("gmt_token")
+                .eq("token", account.getToken());
+        Account result = accountService.getOne(queryWrapperToken);
+        // Token 验证成功
+        return result != null && DateUtils.getDays(new Date(), result.getGmtToken()) < 3;
+    }
+
 
     /**
      * 获取账号 Token
@@ -22,9 +46,13 @@ public class TokenUtils {
         }
     }
 
-    public static Account setAccountToken(Account account) {
+    /**
+     * 设置账号 Token
+     *
+     * @param account 账号
+     */
+    public static void setAccountToken(Account account) {
         account.setToken(getAccountToken(account));
         account.setGmtToken(new Date());
-        return account;
     }
 }
