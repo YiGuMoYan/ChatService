db8087e0d540e8210150981f9c913961d0d3eb80
YiGuMoYan <3194775246@qq.com>
2023-01-14 16:36:12 +0800

    login and register ok

--- src/main/java/top/yigumoyan/chat/controller/AccountController.java
+++ src/main/java/top/yigumoyan/chat/controller/AccountController.java
@@ -1,21 +1,18 @@
 package top.yigumoyan.chat.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import top.yigumoyan.chat.entity.Account;
 import top.yigumoyan.chat.result.Result;
 import top.yigumoyan.chat.service.Impl.AccountServiceImpl;
+import top.yigumoyan.chat.utils.TokenUtils;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 忆古陌烟
@@ -23,31 +20,51 @@ import javax.servlet.http.HttpSession;
  */
 @Controller
 @ResponseBody
+@CrossOrigin
 @RequestMapping("/chat/account")
 public class AccountController {
 
     @Resource
     private AccountServiceImpl accountService;
 
+    /**
+     * 登录
+     *
+     * @param account 前端传 account 对象
+     * @return account 对象
+     */
     @PostMapping("/login")
-    public Result login(HttpSession httpSession , @RequestBody Account account) {
-        System.out.println(account);
+    public Result login(@RequestBody Account account) {
+        // 验证账号密码
         QueryWrapper<Account> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("id", account.getId())
                 .eq("password", account.getPassword());
-        Account one = accountService.getOne(queryWrapper);
-        if (one != null) {
-            httpSession.setAttribute("id", account.getId());
-            return Result.ok(one);
+        Account result = accountService.getOne(queryWrapper);
+        // 判断账号密码是否正确
+        if (result != null) {
+            // 在每次登录时，更新 Token
+            TokenUtils.setAccountToken(result);
+            // 更新成功，允许登录
+            if (accountService.updateById(result)) {
+                return Result.ok(result);
+            }
         }
         return Result.ok(false);
     }
 
+    /**
+     * 注册
+     *
+     * @param account 前端传 account 对象
+     * @return account 对象
+     */
     @PostMapping("/register")
-    public Result register(String password) {
-        Account account = new Account();
-        account.setPassword(password);
-        boolean save = accountService.save(account);
-        return Result.ok(save);
+    public Result register(@RequestBody Account account) {
+        // 给新账号增加 Token
+        TokenUtils.setAccountToken(account);
+        if (accountService.save(account)) {
+            return Result.ok(account);
+        }
+        return Result.ok(false);
     }
 }
